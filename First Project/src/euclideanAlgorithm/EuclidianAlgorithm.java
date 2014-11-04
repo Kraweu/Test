80b4cf352bee9e8c56c05642e08811f950d6d286
Kraweu <kraweu@gmx.at>
2014-11-04 15:24:33 +0100

    Added Comments

--- First Project/src/euclideanAlgorithm/EuclidianAlgorithm.java
+++ First Project/src/euclideanAlgorithm/EuclidianAlgorithm.java
@@ -3,17 +3,23 @@ package euclideanAlgorithm;
 public class EuclidianAlgorithm {
 
 	public static void main(String[] args) {
-
+		//
+		//Initializing
+		//
 		int a = 0, b = 0;
-		
-
+		//
+		//Get Input
+		//
 		System.out.println("Format: a/b a>=b");
 		System.out.println("Enter a: ");
 		a = Input.get_input();
 		System.out.println("Enter b: ");
 		b = Input.get_input();
 		System.out.println("A: " + a + "; B: " + b + ";");
-
+		//
+		//Calculation
+		//
+		
 	}
 
 }
